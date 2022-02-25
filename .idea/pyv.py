ca4a9ffa322c3d3873e7c19efdcf3e4bb152b0c5
Pradip219 <pradip@ht.com>
2022-02-25 18:01:12 +0530

    Turtle

---
+++ .idea/pyv.py
@@ -0,0 +1,5 @@
+import sys
+import requests
+print(sys.path)
+print(sys.executable)
+print(sys.version)
---
+++ Shap.py
@@ -0,0 +1,11 @@
+from turtle import *
+import turtle        
+wn=turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Shapn1")
+shape=turtle.Turtle()
+for i in range(4):
+    shape.left(90)
+    turtle.up
+    shape.forward(100)
+turtle.done()
