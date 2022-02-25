9ca79b752b2805c064d19f309e1d9a10d847d3ae
Pradip219 <pradip@ht.com>
2022-02-25 18:07:48 +0530

    a

--- Shap.py
+++ Shap.py
@@ -4,8 +4,12 @@ wn=turtle.Screen()
 wn.bgcolor("light green")
 wn.title("Shapn1")
 shape=turtle.Turtle()
+shape.fillcolor("red")
+shape.speed(1)
 for i in range(4):
-    shape.left(90)
     turtle.up
+    shape.left(90)
     shape.forward(100)
+    turtle.down
+
 turtle.done()
