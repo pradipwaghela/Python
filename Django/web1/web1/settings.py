54de21f30a06c31deb5c742767f49762944c2a91
Pradip219 <pradip@ht.com>
2022-03-22 19:40:41 +0530

    Django app

--- Django/web1/web1/settings.py
+++ Django/web1/web1/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My App
+    'learning_logs',
+    #Boostrap
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
