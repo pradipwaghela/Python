4040907b77969ffc8faedfec8ca9066c423c658f
Pradip219 <pradip@ht.com>
2022-04-06 19:34:36 +0530

    file

--- Programs/file.py
+++ Programs/file.py
@@ -1,14 +1,33 @@
+"""
+open("Filename","Mode")
+                    r - read only
+                    w - Write only
+                    x - Create if does not exists 
+                    a - Add content to file 
+                    t - text mode default
+                    b - Binary mode
+                    + - Read + Write
+
+"""                 
 import os
 
-try:
-   
-    f= open("mself.txt")
-    line = f.read()
-    if line :
-       print(line)
-    else:
-        print("No data in file ")
-except Exception as e:
-    print(e)
+def ReadFile():
+    try:
+        f= open("mself.txt", "r")
+        line = f.read()
+        if line :
+            print(line)
+        else:
+            print("No data in file ")
+    except Exception as e:
+        print(e)
+def WriteFile(conte):
+    try:
+        f= open("mself.txt", "a")
+        f.write(conte)
+    except Exception as e:
+        print(e)
 
-#print(os.listdir())
+conte=input("Wrtite To file")
+WriteFile(conte)
+ReadFile()
