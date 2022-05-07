db220d1a90ebb3ba95cfa9080ea23c6c92c539a1
Test Pradip <Test Pradip@ht.com>
2022-05-07 14:14:21 +0530

    After long time

---
+++ Projects/tipcal.py
@@ -0,0 +1,5 @@
+print("Welcome to tip calculator.")
+bill=input("What was the total bill? $")
+per=input("What percentage tio would you like to give? 10,20, or 15? ")
+ppl=input("How many people to spilit the bill? ")
+
