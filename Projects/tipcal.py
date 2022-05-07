f97e97dccf44473c30402398ceccb474714b4a7b
Test Pradip <Test Pradip@ht.com>
2022-05-07 14:38:11 +0530

    Bill

--- Projects/tipcal.py
+++ Projects/tipcal.py
@@ -1,5 +1,10 @@
 print("Welcome to tip calculator.")
-bill=input("What was the total bill? $")
-per=input("What percentage tio would you like to give? 10,20, or 15? ")
-ppl=input("How many people to spilit the bill? ")
-
+bill=float(input("What was the total bill? $"))
+tip=int(input("What percentage tio would you like to give? 10,20, or 15? "))
+ppl=int(input("How many people to spilit the bill? "))
+tip_per=tip/100
+tip_amt=bill*tip_per
+bilwtip= bill + tip_amt
+bill_per=bilwtip/ppl
+fnbil=round(bill_per,2)
+print(f" Bill per person is ${fnbil}")
