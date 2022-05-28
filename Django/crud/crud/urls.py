319ed48847872d6cf937c6db6447cacf5c93869c
Test Pradip <Test Pradip@ht.com>
2022-05-28 19:33:53 +0530

    added

--- Django/crud/crud/urls.py
+++ Django/crud/crud/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.urls import path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('',include('person.urls'))
+    url(r'^',include('person.urls'))
 ]
