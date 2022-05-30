3f8d215d092e6102bfcf3dbc521bdc2702e84240
PradipDisha <pradip@disha.dange>
2022-05-30 21:00:52 +0530

    done

--- numpy/first.py
+++ numpy/first.py
@@ -5,6 +5,8 @@ axis - Dimention of array co-odibates
 .size - total elemnt 
 T :- row to column column to row
 .nbytes :- Totoal byte in RAM
+To create array :-
+numpy.array([[]])
 """
 import numpy as np
 import sklearn 
@@ -12,7 +14,11 @@ def sze(aray):
     print("Size of array is ",aray.shape)
     print("Type is ",aray.dtype)
     print(aray)
-myar = np.array([[10,20,30]])
+
+myar = np.array([[10,20,30],[40,50,60],[70,80,90]])
+sze(myar)
+
+"""
 print(myar[0,2])
 print(myar.shape)
 print(myar.dtype)
@@ -20,3 +26,4 @@ a2=np.zeros((3,3))
 print(a2)
 rng=np.arange(10)
 sze(rng)
+"""
